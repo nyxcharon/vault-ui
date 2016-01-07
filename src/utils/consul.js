ad70cd758d255a05ba2fcdb7ac7d008299c4447f
Shane Starcher <shanestarcher@gmail.com>
2016-01-06 19:39:00 -0500

    adding incomplete file

--- src/utils/consul.js
+++ src/utils/consul.js
@@ -4,6 +4,19 @@ const keysMount = `${mount}/logical/ff7e747e-baa3-6739-2c9d-6ec2a044f7ba/`;
 const usersMount = `${mount}/auth/d535374b-ddfd-5dca-76a7-74e28f669e29/user/`;
 
 
+function pathsToObject(paths, result = {}) {
+  for (const path of paths) {
+    const {key, value} = path.split(/_(.+)?/);
+    if(map.has(key)){
+
+    }
+    // buildFromSegments(result, path.split('/'));
+  }
+
+  return result;
+}
+
+
 function fromCallback(fn) {
   return new Bluebird(function(resolve, reject) {
     try {
@@ -38,9 +51,9 @@ export function users() {
 export function keys() {
   return consul.kv.keys(keysMount).spread((data) => {
     const startString = keysMount.length;
-    return data.map((value) => {
+    return pathsToObject(data.map((value) => {
       return value.substring(startString);
-    });
+    }));
   }, (err) => {
     console.log(err);
     return err;
