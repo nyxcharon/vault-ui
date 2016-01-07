bfa02001ba648cc2d26ff9f0e98cbdc61a78db83
Alex Paz <alexjpaz@gmail.com>
2016-01-07 11:54:49 -0500

    Learning MDL. Login redo

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,7 +1,20 @@
 import React, {Component, PropTypes} from 'react';
+
 import {connect} from 'react-redux';
 import * as authActions from 'redux/modules/auth';
-import { Grid, Cell, Card, Textfield, Button } from 'react-mdl';
+import {
+  Grid,
+  Cell,
+  Card,
+  Textfield,
+  CardTitle,
+  CardText,
+  CardActions,
+  CardMenu,
+  IconButton,
+  Button
+} from 'react-mdl';
+
 
 @connect(
   state => ({user: state.auth.user}),
@@ -29,40 +42,58 @@ export default class Login extends Component {
 
   render() {
     const {user} = this.props;
+
+    const containerStyle = {
+      maxWidth: "960px",
+      margin: "40px auto"
+
+    };
+
+    const cardTitleStyle = {
+      marginTop: '20px',
+      height: '200px',
+      background: 'url(https://hashicorp.com/images/blog/vault/list-c8bf47c8.png) center / contain',
+      backgroundRepeat:  'no-repeat'
+    };
+
     return (
-      <Card shadow={0} style={{width: '100%'}}>
-        <Grid>
-          {user &&
-          <Cell col={12}>
-            <h1>You are logged in. Grats.</h1>
-            <Button className="mdl-cell--bottom" style={{float: 'right'}} onClick={this.handleLogout} raised colored ripple>Logout</Button>
-          </Cell>
-          }
-          {!user &&
-          <Cell col={12}>
-            <h1>Login to Vault - UI</h1>
-            <form onSubmit={this.handleSubmit}>
-              <Textfield
-                  floatingLabel
-                  ref="username"
-                  onChange={() => {}}
-                  label="Username"
-                  style={{width: '100%'}}
-              />
-              <Textfield
-                  floatingLabel
-                  ref="password"
-                  onChange={() => {}}
-                  label="Password"
-                  style={{width: '100%'}}
-                  type="password"
-              />
-              <Button className="mdl-cell--bottom" style={{float: 'right'}} type="submit" raised colored ripple>Submit</Button>
-            </form>
-          </Cell>
-          }
-        </Grid>
-      </Card>
+      <div style={containerStyle}>
+      {!user &&
+        <form onSubmit={this.handleSubmit}>
+          <Card shadow={0} style={{width: '512px', margin: 'auto'}}>
+            <div style={cardTitleStyle}></div>
+            <CardTitle>Please sign in</CardTitle>
+            <CardText>
+                <Textfield
+                    floatingLabel
+                    ref="username"
+                    onChange={() => {}}
+                    label="Username"
+                    style={{fontSize: '1em', width: '100%'}}
+                />
+                <Textfield
+                    floatingLabel
+                    ref="password"
+                    onChange={() => {}}
+                    label="Password"
+                    style={{width: '100%'}}
+                    type="password"
+                />
+            </CardText>
+            <CardActions border>
+              <Button style={{float: 'right'}} type="submit" raised ripple>Login</Button>
+            </CardActions>
+          </Card>
+        </form>
+      }
+      {user &&
+        <div>
+          <h1>Signed in bro</h1>
+
+          <Button className="mdl-cell--bottom" style={{float: 'right'}} onClick={this.handleLogout} raised colored ripple>Logout</Button>
+        </div>
+      }
+      </div>
     );
   }
 }
