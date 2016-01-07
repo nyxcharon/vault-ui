70b014eadeccd6c0cbf773fcb99efd3899c624a6
Shane Starcher <shanestarcher@gmail.com>
2016-01-07 15:36:56 -0500

    fixing token

--- api/actions/vault.js
+++ api/actions/vault.js
@@ -1,16 +1,12 @@
-var Vaulted = require('vaulted');
+const Vaulted = require('vaulted');
 
-function getVault(req){
-  const token = {
-    name: req.session.vault_api_token
-  };
-  var myVault = new Vaulted({
-    vault_host: '10.0.10.131',
+function getVault(req) {
+  const myVault = new Vaulted({
+    vault_host: 'vault.service.consul',
     vault_port: 8200,
     vault_ssl: false
   });
-  //myVault.setToken(token);
-  myVault.setToken('');
+  myVault.setToken(req.session.vault_api_token);
   return myVault;
 }
 
@@ -19,12 +15,5 @@ export function health(req) {
 }
 
 export function mounts(req) {
-  var myVault = new Vaulted({
-    vault_host: '10.0.10.131',
-    vault_port: 8200,
-    vault_ssl: false
-  });
-  //myVault.setToken(token);
-  myVault.setToken('');
-  return myVault.getMounts();
+  return getVault(req).getMounts();
 }
