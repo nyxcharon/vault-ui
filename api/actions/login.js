e8176dcce56ae5857c898483361a640ab1427384
Nathan Newman <nathan.newman@threewide.com>
2016-01-08 12:45:12 -0500

    improving logged-in page's ui

--- api/actions/login.js
+++ api/actions/login.js
@@ -1,3 +1,22 @@
+import superagent from 'superagent';
+
+export function login(req) {
+  return new Promise((resolve, reject) => {
+    const usrName = req.body.username;
+    superagent
+      .post(`http://10.0.10.131:8200/v1/auth/userpass/login/${usrName}`)
+      .send({ 'password': req.body.password })
+      .set('Content-Type', 'application/json')
+      .end((err, response) => {
+        if (err) {
+          console.log('Error logging into vault', err);
+          reject({'status': 500, 'message': 'You could not be logged in to vault'});
+        }
+        req.session.vault_api_token = response.body.auth.client_token;
+        resolve({'message': 'success', 'user': usrName});
+      });
+  });
+}
 
 export function loadAuth(req) {
   return new Promise((resolve, reject) => {
