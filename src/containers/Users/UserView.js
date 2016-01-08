b9694ecf133e900e1e4d5df8a7a5e9b7cbc14b7a
Westbrook <awestbrook@ip-192-168-1-34.ec2.internal>
2016-01-08 14:44:18 -0500

    users styling

--- src/containers/Users/UserView.js
+++ src/containers/Users/UserView.js
@@ -29,6 +29,11 @@ export default class UserView extends Component {
     return nextProps.userQuery === this.props.userName;
   }
 
+  resetUser() {
+    console.log('ayy resetting');
+    this.setState(this.defaultProps);
+  }
+
   fetchUser = () => {
     if (!this.props.isLoading) {
       this.props.dispatch(readUser(this.props.userName));
@@ -37,11 +42,10 @@ export default class UserView extends Component {
 
   render() {
     return (<div>
-      <Button onClick={this.fetchUser}>Read!!</Button>
       <p>User Name: {this.props.userName}</p>
       <p>Policy: Awesome</p>
       <p>Changes</p>
-      <Button onClick={this.fetchUser}>Read!</Button>
+      <Button onClick={this.fetchUser} className="mdl-cell--bottom" raised colored ripple>Read!</Button>
       { this.props.isLoading &&
         <Spinner/>
       }
