bfbe35088e5b521314f807e6da73b7a47d21175b
Justin McCarty <jmccarty@ip-192-168-1-37.ec2.internal>
2016-01-07 19:09:03 -0500

    Removed extra routing

--- src/routes.js
+++ src/routes.js
@@ -42,7 +42,6 @@ export default (store) => {
       <Route path="/" component={App}>
         <IndexRoute component={Login}/>
         <Route path="/login" component={Login} />
-        <Route path="/" component={Login} />
 
         <Route onEnter={requireLogin}>
           <Route path="health" component={Health} />
