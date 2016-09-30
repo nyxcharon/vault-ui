744ac1de971caec804485caf8910c3a4c21490e3
Philipp Schmitt <philipp@schmitt.co>
2016-09-30 09:23:58 +0200

    Add support for VAULT_SKIP_VERIFY to skip SSL cert verification

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
 DEBUG = True
 SECRET_KEY = 'thisisfordevelopmentonly'
-VAULT_URL = 'http://localhost:8200'
+VAULT_URL = 'https://vault.dt.ept.lu:8200'
+VAULT_SKIP_VERIFY = True
+AUTH_BACKEND = 'ldap'
